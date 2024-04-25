72615e40777bee1d5da782882b62a047dfcda173
ogison <meyo.ogs9@gmail.com>
2024-04-25 22:23:38 +0900

    Update 指定日に更新したチケットをテキストファイルに出力するメソッドを作成

--- backlog_api.py
+++ backlog_api.py
@@ -1,16 +1,19 @@
 import requests
 import json
 import os
-# import pprint
 from datetime import datetime
+
+file_setting_path = 'setting.json'
+file_user_setting_path = 'user.json'
+directory = 'output'
 
 start_date = '2024-04-22T00:00:00Z'
 end_date = '2024-04-22T23:59:59Z'
-
-file_path = 'setting.json'
 api_key = ''
 project_name = ''
-directory = 'output'
+user_id = ''
+user_name = ''
+
 
 ''' setting_json（設定ファイル）を読み込み
 '''
@@ -20,13 +23,25 @@ def read_setting():
     global api_key
     global project_name
 
-    with open(file_path, 'r') as file:
+    with open(file_setting_path, 'r') as file:
         setting_data = json.load(file)
 
     start_date = setting_data['start_date']
     end_date = setting_data['end_date']
     api_key = setting_data['api_key']
     project_name = setting_data['project_name']
+
+''' user_json（ユーザー設定ファイル）を読み込み
+'''
+def read_user_setting():
+    global user_id
+    global user_name
+
+    with open(file_user_setting_path, 'r') as file:
+        setting_data = json.load(file)
+
+    user_id = setting_data['user_id']
+    user_name = setting_data['user_name']
 
 ''' 日付の比較 
 Args:
@@ -88,11 +103,54 @@ def check_expired_tichket(count):
         with open(file_path, 'w') as f:
             f.write(output_txt)
 
+''' 指定日に更新したチケット一覧をテキストファイルに出力
+Args:
+    count : 確認するチケット数
+'''
+def updated_tichket(count):
+    payload = {
+        'apiKey' : api_key,
+        'count'  : count
+    }
+
+    response = requests.get(f'https://{project_name}.backlog.jp//api/v2/users/{user_id}/activities', payload)
+    json_list = response.json()
+
+    ticket_list = []
+
+    for json in json_list:
+        json_content = json['content']
+        time = json['created']
+
+        if time < start_date or end_date < time:
+            continue
+        if 'changes' in json_content:
+            ticket_id = json_content['id']
+            if ticket_id not in ticket_list:
+                ticket_list.append(ticket_id)
+
+    output_txt = ''
+    for ticket in ticket_list:
+        response_issue = requests.get(f'https://{project_name}.backlog.jp//api/v2/issues/{ticket}', payload)
+        json_issue = response_issue.json()
+        key_id = json_issue['keyId']
+        title = json_issue['summary']
+        output_txt += f'{key_id} : {title}\n'
+    
+    file_path = f'{directory}/{user_name}.txt'
+    if not os.path.exists(file_path):
+        with open(file_path, 'x') as f:
+            f.write(output_txt)
+    else:
+        with open(file_path, 'w') as f:
+            f.write(output_txt)
+
 def main():
-    help(read_setting)
     read_setting()
+    read_user_setting()
     make_directory(directory)
-    check_expired_tichket(10)
+    check_expired_tichket(60)
+    updated_tichket(60)
 
 if __name__ == '__main__':
     main()
